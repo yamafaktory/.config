252efe55bdb4a8be9144b4ddd5778ba1d597ed72
Davy Duperron <yamafaktory@gmail.com>
2024-06-01 19:03:58 +0200

    chore(nvim): update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -393,10 +393,20 @@ local packages = {
     event = 'VeryLazy',
     config = function()
       local neocodeium = require('neocodeium')
+
       neocodeium.setup()
+
       vim.keymap.set('i', '<A-f>', neocodeium.accept)
+      vim.keymap.set('i', '<A-w>', neocodeium.accept_word)
+      vim.keymap.set('i', '<A-a>', neocodeium.accept_line)
+      vim.keymap.set('i', '<A-e>', neocodeium.cycle_or_complete)
+      vim.keymap.set('i', '<A-r>', function()
+        require('neocodeium').cycle_or_complete(-1)
+      end)
+      vim.keymap.set('i', '<A-c>', neocodeium.clear)
     end,
   },
+
   -- Java.
   -- https://github.com/nvim-java/nvim-java/wiki/Lazyvim
   {
