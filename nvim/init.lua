730b4d3bd8c89da97862085c15a87e1ad7fc920a
Davy Duperron <yamafaktory@gmail.com>
2025-07-03 11:46:13 +0200

    fix(nvim+vue): adjust config

--- nvim/init.lua
+++ nvim/init.lua
@@ -555,8 +555,7 @@ local capabilities = require('blink.cmp').get_lsp_capabilities()
 -- List of LSP servers and formatters automatically installed.
 local ensure_installed = {
   -- Servers.
-  -- See this mapping:
-  -- https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
+  -- https://mason-registry.dev/registry/list
   'bash-language-server',
   'css-lsp',
   'dockerfile-language-server',
@@ -567,6 +566,7 @@ local ensure_installed = {
   'json-lsp',
   'just-lsp',
   'lua-language-server',
+  'oxlint',
   'postgrestools',
   'prettier',
   'rust-analyzer',
@@ -575,6 +575,7 @@ local ensure_installed = {
   'tailwindcss-language-server',
   'taplo',
   'typescript-language-server',
+  'vtsls',
   'vue-language-server',
   'yaml-language-server',
   'yamllint',
@@ -691,3 +692,68 @@ config_and_enable('zls', {
 -- Enable some other language servers.
 config_and_enable('just')
 config_and_enable('postgres_lsp')
+
+-- Vue setup.
+-- https://github.com/vuejs/language-tools/wiki/Neovim
+-- sudo npm install -g @vue/typescript-plugin@v2.2.12
+local vue_language_server_path = vim.fn.expand('$MASON/packages')
+  .. '/vue-language-server'
+  .. '/node_modules/@vue/language-server'
+local vue_plugin = {
+  name = '@vue/typescript-plugin',
+  location = vue_language_server_path,
+  languages = { 'vue' },
+  configNamespace = 'typescript',
+}
+local vtsls_config = {
+  settings = {
+    vtsls = {
+      tsserver = {
+        globalPlugins = {
+          vue_plugin,
+        },
+      },
+    },
+  },
+  filetypes = {
+    'typescript',
+    'javascript',
+    'javascriptreact',
+    'typescriptreact',
+    'vue',
+  },
+}
+local vue_ls_config = {
+  on_init = function(client)
+    client.handlers['tsserver/request'] = function(_, result, context)
+      local clients =
+        vim.lsp.get_clients({ bufnr = context.bufnr, name = 'vtsls' })
+      if #clients == 0 then
+        vim.notify(
+          'Could not found `vtsls` lsp client, vue_lsp would not work without it.',
+          vim.log.levels.ERROR
+        )
+        return
+      end
+      local ts_client = clients[1]
+
+      local param = unpack(result)
+      local id, command, payload = unpack(param)
+      ts_client:exec_cmd({
+        title = 'tsserver/request',
+        command = 'typescript.tsserverRequest',
+        arguments = {
+          command,
+          payload,
+        },
+      }, { bufnr = context.bufnr }, function(_, r)
+        local response_data = { { id, r.body } }
+        ---@diagnostic disable-next-line: param-type-mismatch
+        client:notify('tsserver/response', response_data)
+      end)
+    end
+  end,
+}
+vim.lsp.config('vtsls', vtsls_config)
+vim.lsp.config('vue_ls', vue_ls_config)
+vim.lsp.enable({ 'vtsls', 'vue_ls' })
