cdd94e86573fa3736747204ce9147d13dd839796
Davy Duperron <yamafaktory@gmail.com>
2025-12-28 22:16:12 +0100

    chore(configs): update

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
 # Remove greeting message.
-set fish_greeting
+function fish_greeting
+end
 
 # Default editor.
 set -gx EDITOR nvim
@@ -13,9 +16,7 @@ set -gx STARSHIP_LOG error
 starship init fish | source
 
 # Init Atuin.
-# Temporary fix until https://github.com/atuinsh/atuin/issues/2940 is versioned.
-# atuin init fish | source
-atuin init fish | sed "s/-k up/up/g" | source 
+atuin init fish | source
 
 # Bun.
 set --export BUN_INSTALL "$HOME/.bun"
@@ -30,39 +31,44 @@ set PATH $PATH ~/.zvm/bin
 # Neovim Mason bin.
 set -gx PATH $HOME/.local/share/nvim/mason/bin $PATH
 
-# Rosé Pine Moon theme. 
-# syntax highlighting variables
-set -l fish_color_normal e0def4
-set -l fish_color_command c4a7e7
-set -l fish_color_keyword 9ccfd8
-set -l fish_color_quote f6c177
-set -l fish_color_redirection 3e8fb0
-set -l fish_color_end 908caa
-set -l fish_color_error eb6f92
-set -l fish_color_param ea9a97
-set -l fish_color_comment 908caa
-set -l fish_color_selection --reverse
-set -l fish_color_operator e0def4
-set -l fish_color_escape 3e8fb0
-set -l fish_color_autosuggestion 908caa
-set -l fish_color_cwd ea9a97
-set -l fish_color_user f6c177
-set -l fish_color_host 9ccfd8
-set -l fish_color_host_remote c4a7e7
-set -l fish_color_cancel e0def4
-set -l fish_color_search_match --background=232136
-set -l fish_color_valid_path
-# pager color variables
-set -l fish_pager_color_progress ea9a97
-set -l fish_pager_color_background --background=2a273f
-set -l fish_pager_color_prefix 9ccfd8
-set -l fish_pager_color_completion 908caa
-set -l fish_pager_color_description 908caa
-set -l fish_pager_color_secondary_background
-set -l fish_pager_color_secondary_prefix
-set -l fish_pager_color_secondary_completion
-set -l fish_pager_color_secondary_description
-set -l fish_pager_color_selected_background --background=393552
-set -l fish_pager_color_selected_prefix 9ccfd8
-set -l fish_pager_color_selected_completion e0def4
-set -l fish_pager_color_selected_description e0def4
+# Rose Pine Moon theme for fish
+# https://rosepinetheme.com
+# Syntax highlighting color variables
+set -g fish_color_normal e0def4
+set -g fish_color_command c4a7e7
+set -g fish_color_keyword 9ccfd8
+set -g fish_color_quote f6c177
+set -g fish_color_redirection 3e8fb0
+set -g fish_color_end 908caa
+set -g fish_color_error eb6f92
+set -g fish_color_param ea9a97
+set -g fish_color_comment 908caa
+set -g fish_color_selection --reverse
+set -g fish_color_operator e0def4
+set -g fish_color_escape 3e8fb0
+set -g fish_color_autosuggestion 908caa
+set -g fish_color_cwd ea9a97
+set -g fish_color_user f6c177
+set -g fish_color_host 9ccfd8
+set -g fish_color_host_remote c4a7e7
+set -g fish_color_cancel e0def4
+set -g fish_color_search_match --background=232136
+set -g fish_color_valid_path
+# Pager color variables
+set -g fish_pager_color_progress ea9a97
+set -g fish_pager_color_background --background=2a273f
+set -g fish_pager_color_prefix 9ccfd8
+set -g fish_pager_color_completion 908caa
+set -g fish_pager_color_description 908caa
+set -g fish_pager_color_selected_prefix 9ccfd8
+set -g fish_pager_color_selected_completion e0def4
+set -g fish_pager_color_selected_description e0def4
+# Custom color variables
+set -g fish_color_subtle 908caa
+set -g fish_color_text e0def4
+set -g fish_color_love eb6f92
+set -g fish_color_gold f6c177
+set -g fish_color_rose ea9a97
+set -g fish_color_pine 3e8fb0
+set -g fish_color_foam 9ccfd8
+set -g fish_color_iris c4a7e7
--- nvim/init.lua
+++ nvim/init.lua
@@ -35,10 +35,9 @@ vim.wo.signcolumn = 'yes'
 --]]
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
----@diagnostic disable-next-line: undefined-field
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system({
+  local out = vim.fn.system({
     'git',
     'clone',
     '--filter=blob:none',
@@ -46,6 +45,15 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     lazyrepo,
     lazypath,
   })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -212,7 +220,7 @@ local packages = {
       'TSUpdateSync',
     },
     config = function()
-      local configs = require('nvim-treesitter.configs')
+      local configs = require('nvim-treesitter')
 
       ---@diagnostic disable-next-line: missing-fields
       configs.setup({
